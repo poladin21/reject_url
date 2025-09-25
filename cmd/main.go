651044578ad7fc24efe398625d79a9989dcbc1e2
poladin21 <daniilpoladyc@gmail.com>
2025-09-25 19:06:04 +0300

    инициализация хендлера и бд

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,54 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"redirect_url/internal/config"
+	"redirect_url/internal/handlers"
 	"redirect_url/internal/servises"
+	"redirect_url/internal/storegs/url"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
 
 	// config inst
-
 	cfg := config.New()
 
 	// loger inst логер для логирования инфы чекни в консоль там данные можешь посмотреть ошибки нужно это для просмотра всего вышеперечисленного на сервакее
 	loger := newLog(cfg.Loggers.Level)
 
 	// storeg(база данных) inst (дб постгер sql)
+	db, err := NewDB(*cfg)
+	if err != nil {
+		loger.Error("fatl inet db")
+		return
+	}
+
+	storegs := url.New(db)
 
 	// servis inst
-
 	servises := servises.New(nil, loger)
 
 	// interfece (во всех ниже перечисленных)
 
 	// hendler init
 
-	// start servis
+	// Регистрируем обработчик для всех запросов
+	h := handlers.NeHandlers()
 
+	http.HandleFunc("/", h.Get)
+	http.HandleFunc("/exit/", h.Post)
+
+	// start server
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting the server:", err)
+	}
 }
 
 func newLog(level string) *slog.Logger {
@@ -42,4 +63,13 @@ func newLog(level string) *slog.Logger {
 	return slog.New(
 		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: lvl}),
 	)
+}
+
+func NewDB(cfg config.Config) (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		cfg.DB.Host, cfg.DB.Port, cfg.DB.User, cfg.DB.Password, cfg.DB.Name,
+	)
+	return sql.Open("postgres", psqlInfo)
+
 }
